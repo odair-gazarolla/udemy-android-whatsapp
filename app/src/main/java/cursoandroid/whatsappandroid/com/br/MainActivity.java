e9411cfd2fe1649f1fe8f67202363bd55f110487
Odair <odair2502@gmail.com>
2019-09-01 14:02:33 -0300

    Logout method has been implements

--- app/src/main/java/cursoandroid/whatsappandroid/com/br/MainActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/br/MainActivity.java
@@ -1,5 +1,6 @@
 package cursoandroid.whatsappandroid.com.br;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,14 +8,21 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import cursoandroid.whatsappandroid.com.br.config.ConfiguracaoFirebase;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FirebaseAuth usuarioAutenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("WattsApp");
@@ -39,9 +47,21 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.itemSair:
+                deslogarUsuario();
                 return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    public void deslogarUsuario() {
+
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
